4e2a384e7f43b67d238e53b5d7888e48cd4df22c
Tim Beccue <timothybeccue@gmail.com>
2021-04-29 15:47:30 -0700

    Add change to test deploy

--- api/handler.py
+++ api/handler.py
@@ -36,7 +36,7 @@ def dummy_requires_auth(event, context):
 
 def default(event, context):
     log.info(json.dumps(event, indent=2))
-    return http_response(HTTPStatus.OK, "New photon ranch API")
+    return http_response(HTTPStatus.OK, "New photon ranch API, test-deploy")
 
 
 def upload(event, context): 
