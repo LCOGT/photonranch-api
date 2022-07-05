4f2e77526d70dbd14fb067d81402abbbc8abc28a
Darren <darren.e.hunt@gmail.com>
2022-07-05 13:54:59 -0700

    Add docstrings to several files

--- api/helpers.py
+++ api/helpers.py
@@ -19,8 +19,10 @@ ssm_c = boto3.client('ssm', region_name=REGION)
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
-# Helper class to convert a DynamoDB item to JSON.
+
 class DecimalEncoder(json.JSONEncoder):
+    """Helper class to convert a DynamoDB item to JSON."""
+    
     def default(self, o):
         if isinstance(o, decimal.Decimal):
             if o % 1 != 0:
@@ -29,7 +31,10 @@ class DecimalEncoder(json.JSONEncoder):
                 return int(o)
         return super(DecimalEncoder, self).default(o)
 
+
 def http_response(status_code, body):
+    """Returns a given HTTP status code."""
+
     if not isinstance(body, str):
         body = json.dumps(body,cls=DecimalEncoder)
     return {
@@ -39,11 +44,10 @@ def http_response(status_code, body):
             "Access-Control-Allow-Origin": "*",
             # Required for cookies, authorization headers with HTTPS
             "Access-Control-Allow-Credentials": "true",
-            #"Access-Control-Allow-Headers": "*",
-            #"Content-Type": "application/json",
         },
         "body": body
     }
+
 
 def _get_body(event):
     try:
@@ -52,18 +56,24 @@ def _get_body(event):
         log.exception("event body could not be JSON decoded.")
         return {}
 
+
 def get_secret(key):
-    """
+    """Gets environment secrets from the AWS Parameter Store.
+
     Some parameters are stored in AWS Systems Manager Parameter Store.
     This replaces the .env variables we used to use with flask.
     """
+
     resp = ssm_c.get_parameter(
     	Name=key,
     	WithDecryption=True
     )
     return resp['Parameter']['Value']
 
+
 def get_db_connection():
+    """Connects to psycopg2 database."""
+
     connection_params = {
         'host': get_secret('db-host'),
         'database': get_secret('db-database'),
@@ -75,19 +85,31 @@ def get_db_connection():
 
 
 def get_s3_image_path(base_filename, datatype, reduction_level, file_extension):
+    """Gets the path to a file at AWS."""
+
     full_filename = f"{base_filename}-{datatype}{reduction_level}.{file_extension}"
     path = f"data/{full_filename}"
     return path
 
+
 def get_base_filename_from_full_filename(full_filename):
-    ''' convert a string like
-    tst001-inst-20210525-00000123-EX01.fits.bz2
-    to
-    tst001-inst-20210525-00000123
-    '''
+    """Converts a full filename string to its base filename.
+
+    Args:
+        full_filename (str):
+            Name of the file with file extensions
+            (eg. tst001-inst-20210525-00000123-EX01.fits.bz2).
+
+    Returns:
+        Converted filename string in the format tst001-inst-20210525-00000123.
+    """
+
     return full_filename.split('.')[0].rsplit('-', 1)[0]
 
+
 def get_s3_file_url(path, ttl=604800):
+    """Generates a presigned URL for a file at AWS."""
+
     s3 = boto3.client('s3', REGION, config=Config(signature_version='s3v4'))
     url = s3.generate_presigned_url(
         ClientMethod="get_object",
@@ -95,3 +117,4 @@ def get_s3_file_url(path, ttl=604800):
         ExpiresIn=ttl
     )
     return url
+
