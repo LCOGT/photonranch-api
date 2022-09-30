df6463a770adbe0a108cb663d89a94a1f24d388e
Michael Fitzgerald <mfitzasp@gmail.com>
2022-09-30 11:29:28 +1000

    updating tif function to use fz file #1

--- api/s3_helpers.py
+++ api/s3_helpers.py
@@ -33,8 +33,10 @@ def save_tiff_to_s3(bucket, s3_source_key, stretch):
     local_source_file_path = f"/tmp/{uuid.uuid4()}{tmpkey}"
     local_tiff_file_path = f"/tmp/tiff-{tmpkey}.tif"
     local_tiff_file_path_bz2 = f"/tmp/tiff-{tmpkey}.tif.bz2"
+    
+    print (s3_source_key)
 
-    s3_client.download_file(bucket, s3_source_key, local_source_file_path)
+    s3_client.download_file(bucket, s3_source_key + '.fz', local_source_file_path)
     image_metadata = create_tiff(local_source_file_path, local_tiff_file_path, stretch)
 
     # Generate the name for the item in s3; also the name of the downloaded file
