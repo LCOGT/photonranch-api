f7e2a2b228c87dfcd7eb5362a447c10333ded88b
Carolina Capetillo <carolinacapetillo@carolinas-mbp.lco.gtn>
2023-10-19 16:10:16 -0700

    json loaded the string and created a variable for the image header

--- api/db.py
+++ api/db.py
@@ -90,6 +90,7 @@ class Image(Base):
         Notably missing from this is the entire fits header, for smaller 
         payload sizes.
         """
+        header_dictionary = json.loads(self.header)
 
         package = {
             "image_id": self.image_id,
@@ -114,8 +115,8 @@ class Image(Base):
 
             "username": self.username,
             "user_id": self.user_id,
-            "SMARTSTK": self.header.SMARTSTK if self.header is not None else '',
-            "SSTKNUM": self.header.SSTKNUM if self.header is not None else ''
+            "SMARTSTK": header_dictionary.SMARTSTK if self.header is not None else '',
+            "SSTKNUM": header_dictionary.SSTKNUM if self.header is not None else ''
         }
 
         # Convert to timestamp in milliseconds
